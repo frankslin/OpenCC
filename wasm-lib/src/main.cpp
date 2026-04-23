39601018e894c64b1e30e7e79422615c40addfc0
Frank Lin <github@linshuang.info>
2026-04-22 19:15:36 -0700

    Add inspect support to opencc-wasm

--- wasm-lib/src/main.cpp
+++ wasm-lib/src/main.cpp
@@ -1,14 +1,19 @@
 #include <string>
+#include <memory>
 #include <unordered_map>
 
 #include <emscripten/emscripten.h>
 #ifdef OPENCC_WASM_WITH_OPENCC
 #include "../src/opencc.h"
+#include "../src/ConversionInspection.hpp"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
 #endif
 
 struct Converter {
-  opencc_t oc;
+  std::unique_ptr<opencc::SimpleConverter> simple;
   std::string out;
+  std::string inspect_json;
 };
 
 static std::unordered_map<int, Converter> converters;
@@ -27,14 +32,17 @@ int opencc_create(const char* configPath) {
     throw_error("opencc_create: null configPath");
     return -1;
   }
-  opencc_t oc = opencc_open(configPath);
-  if (oc == (opencc_t)-1) {
-    throw_error("opencc_create: opencc_open failed");
+  try {
+    std::unique_ptr<opencc::SimpleConverter> simple(
+        new opencc::SimpleConverter(configPath));
+    int id = next_id++;
+    converters.emplace(
+        id, Converter{std::move(simple), std::string(), std::string()});
+    return id;
+  } catch (const std::exception& ex) {
+    throw_error(ex.what());
     return -1;
   }
-  int id = next_id++;
-  converters.emplace(id, Converter{oc, std::string()});
-  return id;
 #else
   (void)configPath;
   throw_error("opencc_create: OPENCC_WASM_WITH_OPENCC not enabled");
@@ -43,7 +51,6 @@ int opencc_create(const char* configPath) {
 }
 
 const char* opencc_convert(int handle, const char* input) {
-  static std::string err;
 #ifdef OPENCC_WASM_WITH_OPENCC
   if (input == nullptr) {
     return throw_error("opencc_convert: null input");
@@ -52,13 +59,12 @@ const char* opencc_convert(int handle, const char* input) {
   if (it == converters.end()) {
     return throw_error("opencc_convert: invalid handle");
   }
-  char* converted = opencc_convert_utf8(it->second.oc, input, (size_t)-1);
-  if (converted != nullptr) {
-    it->second.out.assign(converted);
-    opencc_convert_utf8_free(converted);
+  try {
+    it->second.out = it->second.simple->Convert(input);
     return it->second.out.c_str();
+  } catch (const std::exception& ex) {
+    return throw_error(ex.what());
   }
-  return throw_error("opencc_convert: conversion returned null");
 #else
   (void)handle;
   (void)input;
@@ -66,11 +72,67 @@ const char* opencc_convert(int handle, const char* input) {
 #endif
 }
 
+const char* opencc_inspect(int handle, const char* input) {
+#ifdef OPENCC_WASM_WITH_OPENCC
+  if (input == nullptr) {
+    return throw_error("opencc_inspect: null input");
+  }
+  auto it = converters.find(handle);
+  if (it == converters.end()) {
+    return throw_error("opencc_inspect: invalid handle");
+  }
+  try {
+    const opencc::ConversionInspectionResult result =
+        it->second.simple->Inspect(input);
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    writer.StartObject();
+    writer.Key("input");
+    writer.String(result.input.data(),
+                  static_cast<rapidjson::SizeType>(result.input.size()));
+    writer.Key("segments");
+    writer.StartArray();
+    for (const auto& segment : result.segments) {
+      writer.String(segment.data(),
+                    static_cast<rapidjson::SizeType>(segment.size()));
+    }
+    writer.EndArray();
+    writer.Key("stages");
+    writer.StartArray();
+    for (const auto& stage : result.stages) {
+      writer.StartObject();
+      writer.Key("index");
+      writer.Uint64(stage.index);
+      writer.Key("segments");
+      writer.StartArray();
+      for (const auto& segment : stage.segments) {
+        writer.String(segment.data(),
+                      static_cast<rapidjson::SizeType>(segment.size()));
+      }
+      writer.EndArray();
+      writer.EndObject();
+    }
+    writer.EndArray();
+    writer.Key("output");
+    writer.String(result.output.data(),
+                  static_cast<rapidjson::SizeType>(result.output.size()));
+    writer.EndObject();
+    it->second.inspect_json.assign(buffer.GetString(), buffer.GetSize());
+    return it->second.inspect_json.c_str();
+  } catch (const std::exception& ex) {
+    return throw_error(ex.what());
+  }
+#else
+  (void)handle;
+  (void)input;
+  return throw_error("opencc_inspect: OPENCC_WASM_WITH_OPENCC not enabled");
+#endif
+}
+
 void opencc_destroy(int handle) {
 #ifdef OPENCC_WASM_WITH_OPENCC
   auto it = converters.find(handle);
   if (it != converters.end()) {
-    opencc_close(it->second.oc);
     converters.erase(it);
   }
 #else
